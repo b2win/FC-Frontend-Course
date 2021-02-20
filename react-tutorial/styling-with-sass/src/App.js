f852745ea7c4b82763a4149c70957efbf8c111da
b2win <68144560+b2win@users.noreply.github.com>
2021-02-20 21:37:08 +0900

    FC API 연동 예제 시작
    FC API 연동 예제 시작

--- react-tutorial/styling-with-sass/src/App.js
+++ react-tutorial/styling-with-sass/src/App.js
@@ -20,6 +20,26 @@ function App() {
       <Button color="pink" size='medium'>BUTTON</Button>
       <Button color="pink" size='small'>BUTTON</Button>
       </div>
+      <div className="buttons">
+      <Button size='large' outline>
+        BUTTON
+        </Button>
+      <Button color="gray" size='medium' outline>BUTTON</Button>
+      <Button color="pink" size='small' outline>BUTTON</Button>
+      </div>
+      <div className="buttons">
+      <Button size='large' fullWidth className="customized-button">
+        BUTTON
+        </Button>
+      <Button color="gray" size='large' fullWidth onClick={() => {
+        console.log('클릭!');
+      }}
+        onMouseMove={() => {
+          console.log('마우스 무브!');
+        }}
+      >BUTTON</Button>
+      <Button color="pink" size='large' fullWidth>BUTTON</Button>
+      </div>
     </div>
   );
 }
--- react-tutorial/styling-with-sass/src/components/Button.js
+++ react-tutorial/styling-with-sass/src/components/Button.js
@@ -2,9 +2,16 @@ import React from 'react';
 import './Button.scss';
 import classNames from 'classnames';
 
-function Button({ children, size, color }) {
+function Button({ children, size, color, outline, fullWidth, className, ...rest }) {
   // return <button className={['Button', size].join{' '}>{children}</button>; 
-  return <button className={classNames('Button', size, color)}>{children}</button>; 
+  return <button className={classNames('Button', size, color, {
+    outline,
+    fullWidth
+  }, className)}
+  {...rest}
+  >
+    {children}
+    </button>; 
 }
 
 Button.defaultProps = {
