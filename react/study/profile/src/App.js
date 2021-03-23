ebc966893bc9e989d74b0d0673629a303fdad035
b2win <68144560+b2win@users.noreply.github.com>
2021-03-23 11:01:54 +0900

    app.js 기본 틀 완성
    app.js 기본 틀 완성

--- react/study/profile/src/App.js
+++ react/study/profile/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+
+  const onType = (e) => {
+    setName(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <input type="text" placeholder="이름" onChange={onType} value={name} />
+        <button onClick={}>추가</button>
+      </div>
+    </>
   );
 }
 
