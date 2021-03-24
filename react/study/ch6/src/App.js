e93b49db3eb727e60a2279f2040880761f4daeb4
b2win <68144560+b2win@users.noreply.github.com>
2021-03-24 16:20:31 +0900

    FC 배열 함수 - 6장 내용 복습 문제 생성
    FC 배열 함수 - 6장 내용 복습 문제 생성

--- react/study/ch6/src/App.js
+++ react/study/ch6/src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import React from "react";
 import Iteration from "./Iteration";
 import IterationSample from "./IterationSample";
+import InputSample from "./InputSample";
 
 function App() {
   return (
     <>
-      <IterationSample />
+      {/* <IterationSample /> */}
+      <InputSample />
     </>
   );
 }
---
+++ react/study/ch6/src/InputSample.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function InputSample() {
+  return (
+    <>
+      <div>
+        <input placeholder="이름" />
+        <input placeholder="닉네임" />
+        <button>초기화</button>
+        <div>
+          <b>값: </b>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default InputSample;
