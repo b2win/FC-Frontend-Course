83e809a759d610ef9f8693a7bfc012a2655be3a3
b2win <68144560+b2win@users.noreply.github.com>
2021-04-05 18:02:29 +0900

    리액트를 다루는 기술 8장 useState예제
    리액트를 다루는 기술 8장 useState예제

--- react/study/life-cycle/src/App.js
+++ react/study/life-cycle/src/App.js
@@ -1,32 +1,11 @@
 import React, { Component } from "react";
-import ErrorBoundary from "./ErrorBoundary";
-import LifeCycleSample from "./LifeCycleSample";
+import Counter from "./Counter";
 
-function getRandomColor() {
-  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+function App() {
+  return (
+    <>
+      <Counter />
+    </>
+  );
 }
-
-class App extends Component {
-  state = {
-    color: "#000000",
-  };
-
-  handleClick = () => {
-    this.setState({
-      color: getRandomColor(),
-    });
-  };
-
-  render() {
-    return (
-      <div>
-        <button onClick={this.handleClick}>랜덤 색상</button>
-        <ErrorBoundary>
-          <LifeCycleSample color={this.state.color} />
-        </ErrorBoundary>
-      </div>
-    );
-  }
-}
-
 export default App;
