63e24a3a02646565e035ee0665c9590d040aaf14
b2win <68144560+b2win@users.noreply.github.com>
2021-04-05 21:29:02 +0900

    8장 useEffect예제
    8장 useEffect예제

--- react/study/life-cycle/src/App.js
+++ react/study/life-cycle/src/App.js
@@ -1,11 +1,22 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import Counter from "./Counter";
 import Info from "./Info";
 
 function App() {
+  const [visible, setVisible] = useState(false);
   return (
     <>
-      <Info />
+      <div>
+        <button
+          onClick={() => {
+            setVisible(!visible);
+          }}
+        >
+          {visible ? "숨기기" : "보이기"}
+        </button>
+        <hr />
+        {visible && <Info />}
+      </div>
     </>
   );
 }
