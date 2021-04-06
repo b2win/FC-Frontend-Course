7e5171be6d4bc848bd8bdb99158c3379e9b22a54
b2win <68144560+b2win@users.noreply.github.com>
2021-04-06 18:08:58 +0900

    커스텀 Hooks 만들기 예제
    커스텀 Hooks 만들기 예제

--- react/study/life-cycle/src/Info.js
+++ react/study/life-cycle/src/Info.js
@@ -1,25 +1,32 @@
 import React, { useReducer } from "react";
+import useInputs from "./useInputs";
 
-function reducer(state, action) {
-  return {
-    ...state,
-    [action.name]: action.value,
-  };
-}
+// function reducer(state, action) {
+//   return {
+//     ...state,
+//     [action.name]: action.value,
+//   };
+// }
 
 function Info() {
-  // const [name, setName] = useState("");
-  // const [nickname, setNickname] = useState("");
-
-  const [state, dispatch] = useReducer(reducer, {
+  const [state, onChange] = useInputs({
     name: "",
     nickname: "",
   });
-
   const { name, nickname } = state;
-  const onChange = (e) => {
-    dispatch(e.target);
-  };
+
+  // const [name, setName] = useState("");
+  // const [nickname, setNickname] = useState("");
+
+  // const [state, dispatch] = useReducer(reducer, {
+  //   name: "",
+  //   nickname: "",
+  // });
+
+  // const { name, nickname } = state;
+  // const onChange = (e) => {
+  //   dispatch(e.target);
+  // };
 
   // useEffect(() => {
   //   // console.log("렌더링이 완료되었습니다!");
