5a5014f7e4dff2e3523bf45daa30efd0f35f3f1f
b2win <68144560+b2win@users.noreply.github.com>
2021-04-09 13:41:07 +0900

    이름입력창 버튼 구현
    이름입력창 버튼 구현

---
+++ react/study/phonebook0409/NameList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NameList() {
+  return (
+    <div>
+      <ul>{nameDetail}</ul>
+    </div>
+  );
+}
+
+export default NameList;
--- react/study/phonebook0409/src/App.js
+++ react/study/phonebook0409/src/App.js
@@ -2,10 +2,23 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [inputName, setInputName] = useState("");
+  const [phonebook, setPhonebook] = useState([]);
+  const [inputName, setInputName] = useState(""); //이름입력창
+  const [nextId, setNextId] = useState("");
 
   const enterName = (e) => {
     setInputName(e.target.value);
+  };
+
+  const onClickAddName = () => {
+    const AddName = phonebook.concat({
+      id: nextId,
+      name: inputName,
+      mobile: "",
+    });
+    setInputName("");
+    setNextId(nextId + 1);
+    setPhonebook(AddName);
   };
 
   return (
@@ -17,7 +30,7 @@ function App() {
         value={inputName}
         onChange={enterName}
       />
-      <button>추가</button>
+      <button onClick={onClickAddName}>추가</button>
     </div>
   );
 }
