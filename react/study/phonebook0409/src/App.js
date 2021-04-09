5aaa35cd7b39da66c164204bcc0efee91c379c76
b2win <68144560+b2win@users.noreply.github.com>
2021-04-09 13:22:40 +0900

    이름 인풋창 구현
    이름 인풋창 구현

--- react/study/phonebook0409/src/App.js
+++ react/study/phonebook0409/src/App.js
@@ -1,10 +1,22 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [inputName, setInputName] = useState("");
+
+  const enterName = (e) => {
+    setInputName(e.target.value);
+  };
+
   return (
     <div>
       <p>이름입력</p>
-      <input type="text" placeholder="성함을 입력해주세요!" />
+      <input
+        type="text"
+        placeholder="성함을 입력해주세요!"
+        value={inputName}
+        onChange={enterName}
+      />
       <button>추가</button>
     </div>
   );
