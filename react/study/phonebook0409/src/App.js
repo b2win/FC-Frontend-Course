3244ac29206090d9aebf7e5a3167dcffa5ed0a6e
b2win <68144560+b2win@users.noreply.github.com>
2021-04-09 13:05:04 +0900

    전화번호부 폴더생성 및 이름 인풋 추가
    전화번호부 폴더생성 및 이름 인풋 추가

---
+++ react/study/phonebook0409/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <p>이름입력</p>
+      <input type="text" placeholder="성함을 입력해주세요!" />
+      <button>추가</button>
+    </div>
+  );
+}
+
+export default App;
