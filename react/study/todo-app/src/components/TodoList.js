07301e08beaa11c2a4d911adc476131762c86665
b2win <68144560+b2win@users.noreply.github.com>
2021-04-11 17:52:33 +0900

    10장 todo 기능 구현 완료
    10장 todo 기능 구현 완료

--- react/study/todo-app/src/components/TodoList.js
+++ react/study/todo-app/src/components/TodoList.js
@@ -2,12 +2,17 @@ import React from 'react';
 import TodoListItem from './TodoListItem';
 import './TodoList.scss';
 
-function TodoList() {
+function TodoList({ todos, onRemove, onToggle }) {
   return (
-    <div>
-      <TodoListItem />
-      <TodoListItem />
-      <TodoListItem />
+    <div className="TodoList">
+      {todos.map((todo) => (
+        <TodoListItem
+          todo={todo}
+          key={todo.id}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
+      ))}
     </div>
   );
 }
