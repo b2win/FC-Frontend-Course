a54b1780a7fdd49e4d4dad6d395fea963a3e0c98
b2win <68144560+b2win@users.noreply.github.com>
2021-04-14 22:59:09 +0900

    사이드바 리스트 map함수로 변경
    사이드바 리스트 map함수로 변경

--- react/study/kiosk/src/App.js
+++ react/study/kiosk/src/App.js
@@ -2,16 +2,22 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const category = [
+    "버거",
+    "맥카페 & 음료",
+    "사이드 & 디저트",
+    "맥모닝",
+    "맥런치",
+  ];
+  const categoryList = category.map((list) => <li>{list}</li>);
+
   return (
     <div>
       <h1>메뉴</h1>
-      <ul style={{ listStyle: "none" }}>
-        <li>버거</li>
-        <li>맥카페 &amp; 음료</li>
-        <li>사이드 &amp; 디저트</li>
-        <li>맥모닝</li>
-        <li>맥런치</li>
-      </ul>
+      <div className="sideBar">
+        <ul style={{ listStyle: "none" }}>{categoryList}</ul>
+      </div>
+      <div></div>
     </div>
   );
 }
