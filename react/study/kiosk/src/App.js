f5acb1b656bab09d6bcef1b01c8086c165571f8d
b2win <68144560+b2win@users.noreply.github.com>
2021-04-14 22:24:34 +0900

    사이드 바 리스트 생성
    사이드 바 리스트 생성

--- react/study/kiosk/src/App.js
+++ react/study/kiosk/src/App.js
@@ -1,8 +1,19 @@
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>메뉴</h1>
+      <ul style={{ listStyle: "none" }}>
+        <li>버거</li>
+        <li>맥카페 &amp; 음료</li>
+        <li>사이드 &amp; 디저트</li>
+        <li>맥모닝</li>
+        <li>맥런치</li>
+      </ul>
+    </div>
+  );
 }
 
 export default App;
