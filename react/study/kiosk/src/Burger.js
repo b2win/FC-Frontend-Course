b7964283991a9ffb1e2808ea76c0dd31fcc34fff
b2win <68144560+b2win@users.noreply.github.com>
2021-04-15 12:13:22 +0900

    버거 컴포넌트 burger 객체, burgerlist 함수 생성
    버거 컴포넌트 burger 객체, burgerlist 함수 생성

--- react/study/kiosk/src/Burger.js
+++ react/study/kiosk/src/Burger.js
@@ -1,41 +1,66 @@
 import React from "react";
 
 function Burger() {
+  const burger = [
+    {
+      id: 1,
+      nameKor: "필레 오 피쉬",
+      nameEng: "Filet-O-Fish",
+      kcal: "342kcal",
+      price: 100,
+    },
+    {
+      id: 2,
+      nameKor: "더블 필레오피쉬",
+      nameEng: "Double Filet-O-Fish",
+      kcal: "481kcal",
+      price: 200,
+    },
+    {
+      id: 3,
+      nameKor: "1955® 버거",
+      nameEng: "1955® Burger",
+      kcal: "536kcal",
+      price: 300,
+    },
+    {
+      id: 4,
+      nameKor: "빅맥",
+      nameEng: "Big mac",
+      kcal: "583kcal",
+      price: 400,
+    },
+    {
+      id: 5,
+      nameKor: "맥스파이시® 상하이 버거",
+      nameEng: "McSpicy® Shanghai Burger",
+      kcal: "494kcal",
+      price: 500,
+    },
+    {
+      id: 6,
+      nameKor: "맥치킨®",
+      nameEng: "McChicken®",
+      kcal: "483kcal",
+      price: 600,
+    },
+  ];
+
+  const burgerList = burger.map((list) => (
+    <>
+      <li key={list.id}>
+        <div>{list.nameKor}</div>
+        <div>{list.nameEng}</div>
+        <div>{list.kcal}</div>
+      </li>
+      <br />
+    </>
+  ));
+
   return (
     <div>
       <h1>버거</h1>
-      <ul>
-        <li>
-          <div className="kor-name">필레 오 피쉬</div>
-          <div className="eng-name">Filet-O-Fish</div>
-          <div>342kcal</div>
-        </li>
-        <li>
-          <div className="kor-name">더블 필레오피쉬</div>
-          <div className="eng-name">Double Filet-O-Fish</div>
-          <div>481kcal</div>
-        </li>
-        <li>
-          <div className="kor-name">1955® 버거</div>
-          <div className="eng-name">1955® Burger</div>
-          <div>536kcal</div>
-        </li>
-        <li>
-          <div className="kor-name">빅맥</div>
-          <div className="eng-name">Big mac</div>
-          <div>583kcal</div>
-        </li>
-        <li>
-          <div className="kor-name">맥스파이시® 상하이 버거</div>
-          <div className="eng-name">McSpicy® Shanghai Burger</div>
-          <div>494kcal</div>
-        </li>
-        <li>
-          <div className="kor-name">맥치킨®</div>
-          <div className="eng-name">McChicken®</div>
-          <div>483kcal</div>
-        </li>
-      </ul>
+      <ul>{burgerList}</ul>
     </div>
   );
 }
