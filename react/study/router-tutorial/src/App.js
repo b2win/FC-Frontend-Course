5ec00dea306a88895eb72d99ce5535532ace8be3
b2win <68144560+b2win@users.noreply.github.com>
2021-04-20 12:54:12 +0900

    13장 리액트 라우터 부가 기능 (history) 예제
    13장 리액트 라우터 부가 기능 (history) 예제

--- react/study/router-tutorial/src/App.js
+++ react/study/router-tutorial/src/App.js
@@ -4,6 +4,7 @@ import { Route, Link } from "react-router-dom";
 import About from "./About";
 import Home from "./Home";
 import Profiles from "./Profiles";
+import HistorySample from "./HistorySample";
 
 function App() {
   return (
@@ -18,11 +19,15 @@ function App() {
         <li>
           <Link to="/profiles">프로필</Link>
         </li>
+        <li>
+          <Link to="/history">History 예제</Link>
+        </li>
       </ul>
       <hr />
       <Route path="/" component={Home} exact={true} />
       <Route path={["/About", "/Info"]} component={About} />
       <Route path="/profiles" component={Profiles} />
+      <Route path="/history" component={HistorySample} />
     </div>
   );
 }
