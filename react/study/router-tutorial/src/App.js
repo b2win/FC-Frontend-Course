503c7c2de5223ecb544c90cc790e49285dcbd268
b2win <68144560+b2win@users.noreply.github.com>
2021-04-20 12:40:42 +0900

    13장 서브 라우트 예제
    13장 서브 라우트 예제

--- react/study/router-tutorial/src/App.js
+++ react/study/router-tutorial/src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import { Route, Link } from "react-router-dom";
 import About from "./About";
 import Home from "./Home";
-import Profile from "./Profile";
+import Profiles from "./Profiles";
 
 function App() {
   return (
@@ -16,16 +16,13 @@ function App() {
           <Link to="/about">소개</Link>
         </li>
         <li>
-          <Link to="/profile/velopert">velopert 프로필</Link>
-        </li>
-        <li>
-          <Link to="/profile/b2win">b2win 프로필</Link>
+          <Link to="/profiles">프로필</Link>
         </li>
       </ul>
       <hr />
       <Route path="/" component={Home} exact={true} />
       <Route path={["/About", "/Info"]} component={About} />
-      <Route path="/profile/:username" component={Profile} />
+      <Route path="/profiles" component={Profiles} />
     </div>
   );
 }
