5ad28bbc04e873611ffb2b98afa2be688c04f21b
b2win <68144560+b2win@users.noreply.github.com>
2021-04-20 18:21:02 +0900

    14.4 뉴스 뷰어 UI 만들기 예제
    14.4 뉴스 뷰어 UI 만들기 예제

--- react/study/news-viewer/src/App.js
+++ react/study/news-viewer/src/App.js
@@ -1,33 +1,33 @@
-import axios from "axios";
-import React, { useState } from "react";
+import React from "react";
+// import axios from "axios";
+import NewsList from "./components/NewsList";
 
 function App() {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try {
-      const response = await axios.get(
-        "https://newsapi.org/v2/top-headlines?country=kr&apiKey=cb7f2c87059e431aac872c465d1287bd"
-      );
-      setData(response.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
+  // const [data, setData] = useState(null);
+  // const onClick = async () => {
+  //   try {
+  //     const response = await axios.get(
+  //       "https://newsapi.org/v2/top-headlines?country=kr&apiKey=cb7f2c87059e431aac872c465d1287bd"
+  //     );
+  //     setData(response.data);
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // };
 
-  return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && (
-        <textarea
-          rows={7}
-          value={JSON.stringify(data, null, 2)}
-          readOnly={true}
-        />
-      )}
-    </div>
-  );
+  return <NewsList />;
+  // <div>
+  //   <div>
+  //     <button onClick={onClick}>불러오기</button>
+  //   </div>
+  //   {data && (
+  //     <textarea
+  //       rows={7}
+  //       value={JSON.stringify(data, null, 2)}
+  //       readOnly={true}
+  //     />
+  //   )}
+  // </div>
 }
 
 export default App;
