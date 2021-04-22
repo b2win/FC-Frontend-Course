e771400e31172beedb4aa1aefe65c0b12e64ee31
b2win <68144560+b2win@users.noreply.github.com>
2021-04-22 17:44:51 +0900

    14장 NewsList 컴포넌트 수정
    14장 NewsList 컴포넌트 수정

--- react/study/news-viewer/src/components/NewsList.js
+++ react/study/news-viewer/src/components/NewsList.js
@@ -45,3 +45,63 @@ function NewsList({ category }) {
 }
 
 export default NewsList;
+
+// import React, { useEffect } from "react";
+// import styled from "styled-components";
+// import NewsItem from "./NewsItem";
+// import axios from "axios";
+
+// const NewsListBlock = styled.div`
+//   box-sizing: border-box;
+//   padding-bottom: 3rem;
+//   width: 758px;
+//   margin: 0 auto;
+//   margin-top: 2rem;
+//   @media screen and (max-width: 768px) {
+//     width: 100%;
+//     padding-left: 1rem;
+//     padding-right: 1rem;
+//   }
+// `;
+
+// function NewsList({ category }) {
+//   const [articles, setArticles] = useState(null);
+//   const [loading, setLoading] = useState(false);
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       setLoading(true);
+//       try {
+//         const query = category === "all" ? "" : `&category=${category}`;
+//         const response = await axios.get(
+//           `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=cb7f2c87059e431aac872c465d1287bd`
+//         );
+//         setArticles(response.data.articles);
+//       } catch (e) {
+//         console.log(e);
+//       }
+//       setLoading(false);
+//     };
+//     fetchData();
+//   }, [category]);
+
+//   if (loading) {
+//     return <NewsListBlock>대기 중...</NewsListBlock>;
+//   }
+//   if (!response) {
+//     return null;
+//   }
+//   if (error) {
+//     return <NewsListBlock>에러 발생!</NewsListBlock>;
+//   }
+//   const { articles } = response.data;
+//   return (
+//     <NewsListBlock>
+//       {articles.map((article) => (
+//         <NewsItem key={article.url} article={article} />
+//       ))}
+//     </NewsListBlock>
+//   );
+// }
+
+// export default NewsList;
