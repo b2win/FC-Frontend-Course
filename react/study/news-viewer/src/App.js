98a55195e85be35202e28c647912d3ec1b3559ad
b2win <68144560+b2win@users.noreply.github.com>
2021-04-25 15:15:05 +0900

    useEffect 예제 복습 코드
    useEffect 예제 복습 코드

--- react/study/news-viewer/src/App.js
+++ react/study/news-viewer/src/App.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import React from "react";
 // import Categories from "./components/Categories";
 // import NewsList from "./components/NewsList";
 import NewsPage from "./pages/NewsPage";
--- react/study/practice/src/Counter.js
+++ react/study/practice/src/Counter.js
@@ -1,37 +1,71 @@
-import React, { useState} from 'react';
+// import React, { useState} from 'react';
+
+// function Counter() {
+
+//   const [number, setNumber] = useState(0);
+
+//   const onIncrease = () => {
+//     setNumber(number + 1)
+//   };
+
+//   const onDecrease = () => {
+//     setNumber(number - 1)
+//   };
+
+//   const black = {
+//     color: 'black'
+//   }
+
+//   const positive =  {
+//     color: 'blue'
+//   }
+
+//   const negative = {
+//     color: 'red'
+//   }
+
+//   return(
+//     <div>
+//       <h1 style={number >= 0 ? positive : negative }><span>{number}</span></h1>
+//       <button onClick={onIncrease}>+1</button>
+//       <button onClick={onDecrease}>-1</button>
+//     </div>
+//   )
+// }
+
+// export default Counter;
+
+import React, { useReducer } from "react";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    default:
+      return state;
+  }
+}
 
 function Counter() {
-
-  const [number, setNumber] = useState(0);
+  const [number, dispatch] = useReducer(reducer, 0);
 
   const onIncrease = () => {
-    setNumber(number + 1)
+    dispatch({ type: "INCREMENT" });
   };
 
   const onDecrease = () => {
-    setNumber(number - 1)
+    dispatch({ type: "DECREMENT" });
   };
 
-  const black = {
-    color: 'black'
-  }
-
-  const positive =  {
-    color: 'blue'
-  }
-
-  const negative = {
-    color: 'red'
-  }
-
-  return(
+  return (
     <div>
-      <h1 style={number >= 0 ? positive : negative }><span>{number}</span></h1>
+      <h1>{number}</h1>
       <button onClick={onIncrease}>+1</button>
       <button onClick={onDecrease}>-1</button>
     </div>
-  )
+  );
 }
-
 
 export default Counter;
