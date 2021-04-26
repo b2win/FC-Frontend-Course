e0b796e7cf7cf571973979587d42b43ca332ddf2
b2win <68144560+b2win@users.noreply.github.com>
2021-04-26 18:12:46 +0900

    Link 컴포넌트 추가
    Link 컴포넌트 추가

--- react/study/router0426/src/App.js
+++ react/study/router0426/src/App.js
@@ -1,12 +1,21 @@
-import { Route } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
 
 function App() {
   return (
     <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="About">소개</Link>
+        </li>
+      </ul>
+      <hr />
       <Route path="/" component={Home} exact />
-      <Route path="/about" component={About} />
+      <Route path={["/about", "/info"]} component={About} />
     </div>
   );
 }
