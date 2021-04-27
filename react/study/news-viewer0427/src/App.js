184407320a72287f24c175b9cd8a7579fd5c517e
b2win <68144560+b2win@users.noreply.github.com>
2021-04-27 20:23:07 +0900

    14.6 카테고리 UI 생성 예제 복습
    14.6 카테고리 UI 생성 예제 복습

--- react/study/news-viewer0427/src/App.js
+++ react/study/news-viewer0427/src/App.js
@@ -1,30 +1,13 @@
-import { useState } from "react";
-import axios from "axios";
-import NewsList from "./NewsList";
+import React from "react";
+import NewsList from "./components/NewsList";
+import Categories from "./components/Categories";
 
 function App() {
-  // const [data, setData] = useState(null);
-  // const onClick = async () => {
-  //   try {
-  //     const response = await axios.get(
-  //       "https://newsapi.org/v2/top-headlines?country=kr&apikey=cb7f2c87059e431aac872c465d1287bd"
-  //     );
-  //     setData(response.data);
-  //   } catch (e) {
-  //     console.log(e);
-  //   }
-  // };
-
   return (
-    <NewsList />
-    // <div>
-    //   <div>
-    //     <button onClick={onClick}>불러오기</button>
-    //   </div>
-    //   {data && (
-    //     <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly />
-    //   )}
-    // </div>
+    <>
+      <Categories />
+      <NewsList />
+    </>
   );
 }
 
