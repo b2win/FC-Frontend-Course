73d31ee4c2fcdd72ae0303eacde161280ca863df
b2win <68144560+b2win@users.noreply.github.com>
2021-04-27 20:10:59 +0900

    14.4 뉴스 뷰어 UI 예제 복습
    14.4 뉴스 뷰어 UI 예제 복습

--- react/study/news-viewer0427/src/App.js
+++ react/study/news-viewer0427/src/App.js
@@ -1,27 +1,30 @@
-import logo from "./logo.svg";
-import "./App.css";
 import { useState } from "react";
 import axios from "axios";
+import NewsList from "./NewsList";
 
 function App() {
-  const [data, setData] = useState(null);
-  const onClick = () => {
-    axios
-      .get("https://jsonplaceholder.typicode.com/todos/1")
-      .then((response) => {
-        setData(response.data);
-      });
-  };
+  // const [data, setData] = useState(null);
+  // const onClick = async () => {
+  //   try {
+  //     const response = await axios.get(
+  //       "https://newsapi.org/v2/top-headlines?country=kr&apikey=cb7f2c87059e431aac872c465d1287bd"
+  //     );
+  //     setData(response.data);
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // };
 
   return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && (
-        <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly />
-      )}
-    </div>
+    <NewsList />
+    // <div>
+    //   <div>
+    //     <button onClick={onClick}>불러오기</button>
+    //   </div>
+    //   {data && (
+    //     <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly />
+    //   )}
+    // </div>
   );
 }
 
---
+++ react/study/news-viewer0427/src/NewsList.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import NewsItem from "./NewsItems";
+import axios from "axios";
+
+const NewsListBlock = styled.div`
+  box-sizing: border-box;
+  padding-bottom: 3rem;
+  width: 768px;
+  margin: 0 auto;
+  margin-top: 2rem;
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+`;
+
+function NewsList() {
+  const [articles, setArticles] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get(
+          "https://newsapi.org/v2/top-headlines?country=kr&apikey=cb7f2c87059e431aac872c465d1287bd"
+        );
+        setArticles(response.data.articles);
+      } catch (e) {
+        console.log(e);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <NewsListBlock>대기중 ... </NewsListBlock>;
+  }
+  if (!articles) {
+    return null;
+  }
+
+  return (
+    <NewsListBlock>
+      {articles.map((article) => (
+        <NewsItem key={article.url} article={article} />
+      ))}
+    </NewsListBlock>
+  );
+}
+
+export default NewsList;
