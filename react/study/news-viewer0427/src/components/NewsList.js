5cba33f6c29e46168140eed77deb8de3daaf079d
b2win <68144560+b2win@users.noreply.github.com>
2021-04-27 20:54:39 +0900

    14.6.2 예제 복습
    14.6.2 예제 복습

--- react/study/news-viewer0427/src/components/NewsList.js
+++ react/study/news-viewer0427/src/components/NewsList.js
@@ -16,7 +16,7 @@ const NewsListBlock = styled.div`
   }
 `;
 
-function NewsList() {
+function NewsList({ category }) {
   const [articles, setArticles] = useState(null);
   const [loading, setLoading] = useState(false);
 
@@ -24,8 +24,9 @@ function NewsList() {
     const fetchData = async () => {
       setLoading(true);
       try {
+        const query = category === "all" ? "" : `&category=${category}`;
         const response = await axios.get(
-          "https://newsapi.org/v2/top-headlines?country=kr&apikey=cb7f2c87059e431aac872c465d1287bd"
+          `https://newsapi.org/v2/top-headlines?country=kr${query}&apikey=cb7f2c87059e431aac872c465d1287bd`
         );
         setArticles(response.data.articles);
       } catch (e) {
@@ -34,7 +35,7 @@ function NewsList() {
       setLoading(false);
     };
     fetchData();
-  }, []);
+  }, [category]);
 
   if (loading) {
     return <NewsListBlock>대기중 ... </NewsListBlock>;
