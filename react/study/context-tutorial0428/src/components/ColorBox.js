08839c2d8fa46acd316d784d6b9c10a5d5b00691
b2win <68144560+b2win@users.noreply.github.com>
2021-05-01 00:23:43 +0900

    15.4 useContext Hook 예제 복습
    15.4 useContext Hook 예제 복습

--- react/study/context-tutorial0428/src/components/ColorBox.js
+++ react/study/context-tutorial0428/src/components/ColorBox.js
@@ -1,28 +1,45 @@
-import React from "react";
+import React, { useContext } from "react";
 import ColorContext from "../context/color";
 
 function ColorBox() {
+  const { state } = useContext(ColorContext);
   return (
-    <ColorContext.Consumer>
-      {({ state }) => (
-        <>
-          <div
-            style={{
-              width: "200px",
-              height: "200px",
-              background: state.color,
-            }}
-          />
-          <div
-            style={{
-              width: "150px",
-              height: "150px",
-              background: state.subcolor,
-            }}
-          />
-        </>
-      )}
-    </ColorContext.Consumer>
+    <>
+      <div
+        style={{
+          width: "200px",
+          height: "200px",
+          background: state.color,
+        }}
+      />
+      <div
+        style={{
+          width: "150px",
+          height: "150px",
+          background: state.subcolor,
+        }}
+      />
+    </>
+    //   <ColorContext.Consumer>
+    //   {({ state }) => (
+    //     <>
+    //       <div
+    //         style={{
+    //           width: "200px",
+    //           height: "200px",
+    //           background: state.color,
+    //         }}
+    //       />
+    //       <div
+    //         style={{
+    //           width: "150px",
+    //           height: "150px",
+    //           background: state.subcolor,
+    //         }}
+    //       />
+    //     </>
+    //   )}
+    // </ColorContext.Consumer>
   );
 }
 
