5246310cc8d65dbd77020c272894ed35c8f44630
b2win <68144560+b2win@users.noreply.github.com>
2021-05-10 18:28:50 +0900

    17장 관련 FC 강의 예제 코드
    17장 관련 FC 강의 예제 코드

---
+++ react/study/learn-redux/src/containers/CounterContainer.js
@@ -0,0 +1,47 @@
+import React from "react";
+import Counter from "../components/Counter";
+import { connect } from "react-redux";
+import { decrease, increase, setDiff } from "../modules/counter";
+
+function CounterContainer({ number, diff, increase, decrease, setDiff }) {
+  return (
+    <div>
+      <Counter
+        number={number}
+        diff={diff}
+        onIncrease={increase}
+        onDecrease={decrease}
+        onSetDiff={setDiff}
+      />
+    </div>
+  );
+}
+
+const mapStateToProps = (state) => ({
+  number: state.counter.number,
+  diif: state.counter.diff,
+});
+
+// const mapDispatchToProps = (dispatch) => ({
+//   onIncrease: () => dispatch(increase()),
+//   onDecrease: () => dispatch(decrease()),
+//   onSetDiff: (diff) => dispatch(diff),
+// });
+
+// const mapDispatchToProps = (dispatch) =>
+//   bindActionCreators(
+//     {
+//       increase,
+//       decrease,
+//       setDiff,
+//     },
+//     dispatch
+//   );
+
+const mapDispatchToProps = {
+  increase,
+  decrease,
+  setDiff,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
