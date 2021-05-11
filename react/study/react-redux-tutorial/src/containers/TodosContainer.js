86fea3a9084fe4fdfac7c4baa0c2aaa1dfa970d1
b2win <68144560+b2win@users.noreply.github.com>
2021-05-11 16:17:21 +0900

    17.7장 예제 코드
    17.7장 예제 코드

--- react/study/react-redux-tutorial/src/containers/TodosContainer.js
+++ react/study/react-redux-tutorial/src/containers/TodosContainer.js
@@ -1,30 +1,38 @@
 import React from "react";
-import { connect } from "react-redux";
+import { useSelector } from "react-redux";
 import Todos from "../components/Todos";
 import { changeInput, insert, toggle, remove } from "../modules/todos";
+import useActions from "../lib/useActions";
 
-function TodosContainer({ input, todos, changeInput, insert, toggle, remove }) {
+function TodosContainer() {
+  const { input, todos } = useSelector(({ todos }) => ({
+    input: todos.input,
+    todos: todos.todos,
+  }));
+
+  // const dispatch = useDispatch();
+  // const onChangeInput = useCallback(
+  //   (input) => dispatch(changeInput(input)),
+  //   [dispatch]
+  // );
+  // const onInsert = useCallback((text) => dispatch(insert(text)), [dispatch]);
+  // const onToggle = useCallback((id) => dispatch(toggle(id)), [dispatch]);
+  // const onRemove = useCallback((id) => dispatch(remove(id)), [dispatch]);
+
+  const [onChangeInput, onInsert, onToggle, onRemove] = useActions(
+    [changeInput, insert, toggle, remove],
+    []
+  );
   return (
     <Todos
       input={input}
       todos={todos}
-      onChangeInput={changeInput}
-      onToggle={toggle}
-      onInsert={insert}
-      onRemove={remove}
+      onChangeInput={onChangeInput}
+      onToggle={onToggle}
+      onInsert={onInsert}
+      onRemove={onRemove}
     />
   );
 }
 
-export default connect(
-  ({ todos }) => ({
-    input: todos.input,
-    todos: todos.todos,
-  }),
-  {
-    changeInput,
-    insert,
-    toggle,
-    remove,
-  }
-)(TodosContainer);
+export default React.memo(TodosContainer);
--- react/study/react-redux-tutorial/src/modules/todos.js
+++ react/study/react-redux-tutorial/src/modules/todos.js
@@ -1,5 +1,5 @@
 import { createAction, handleActions } from "redux-actions";
-import produce form 'immer';
+import produce from "immer";
 
 const CHANGE_INPUT = "todos/CHANGE_INPUT";
 const INSERT = "todos/INSERT";
@@ -50,20 +50,24 @@ const initialState = {
 
 const todos = handleActions(
   {
-    [CHANGE_INPUT]: (state, { payload: input }) => produce(state, draft => {
-      draft.input = input;
-    }),
-    [INSERT]: (state, { payload: todo }) => produce(state, draft => {
-      draft.todos.push(todo);
-    }),
-    [TOGGLE]: (state, { payload: id }) => produce(state, draft => {
-      const todo = draft.todos.find(todo => todo.id === id);
-      todo.done = !todo.done;
-    }),
-    [REMOVE]: (state, { payload: id }) => produce(state, draft => {
-      const index = draft.todos.findIndex(todo => todo.id === id);
-      draft.todos.splice(index, 1);
-    })
+    [CHANGE_INPUT]: (state, { payload: input }) =>
+      produce(state, (draft) => {
+        draft.input = input;
+      }),
+    [INSERT]: (state, { payload: todo }) =>
+      produce(state, (draft) => {
+        draft.todos.push(todo);
+      }),
+    [TOGGLE]: (state, { payload: id }) =>
+      produce(state, (draft) => {
+        const todo = draft.todos.find((todo) => todo.id === id);
+        todo.done = !todo.done;
+      }),
+    [REMOVE]: (state, { payload: id }) =>
+      produce(state, (draft) => {
+        const index = draft.todos.findIndex((todo) => todo.id === id);
+        draft.todos.splice(index, 1);
+      }),
   },
   initialState
 );
