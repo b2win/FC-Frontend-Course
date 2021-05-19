686d5f87a11472b85344de09d52461ff02b90f7a
b2win <68144560+b2win@users.noreply.github.com>
2021-05-19 19:03:18 +0900

    18장 리덕스 미들웨어 thunk 예제 복습
    18장 리덕스 미들웨어 thunk 예제 복습 및 다른 코드로 작성

---
+++ react/study/learn-redux-middleware0518/src/containers/SampleContainer.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { connect, useDispatch, useSelector } from "react-redux";
+import Sample from "../components/Sample";
+import { getPost, getUsers } from "../modules/sample";
+
+const { useEffect } = React;
+const SampleContainer = () => {
+  const { post, users, loadingPost, loadingUsers } = useSelector(
+    ({ sample }) => ({
+      post: sample.post,
+      users: sample.users,
+      loadingPost: sample.loading.GET_POST,
+      loadingUsers: sample.loading.GET_USERS,
+    })
+  );
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPost(1));
+    dispatch(getUsers(1));
+  }, [dispatch]);
+  return (
+    <Sample
+      post={post}
+      users={users}
+      loadingPost={loadingPost}
+      loadingUsers={loadingUsers}
+    />
+  );
+};
+
+export default SampleContainer;
+
+// const { useEffect } = React;
+// const SampleContainer = ({
+//   getPost,
+//   getUsers,
+//   post,
+//   users,
+//   loadingPost,
+//   loadingUsers,
+// }) => {
+//   // 클래스 형태 컴포넌트였다면 componentDidMount
+//   useEffect(() => {
+//     getPost(1);
+//     getUsers(1);
+//   }, [getPost, getUsers]);
+//   return (
+//     <Sample
+//       post={post}
+//       users={users}
+//       loadingPost={loadingPost}
+//       loadingUsers={loadingUsers}
+//     />
+//   );
+// };
+
+// export default connect(
+//   ({ sample }) => ({
+//     post: sample.post,
+//     users: sample.users,
+//     loadingPost: sample.loading.GET_POST,
+//     loadingUsers: sample.loading.GET_USERS,
+//   }),
+//   {
+//     getPost,
+//     getUsers,
+//   }
+// )(SampleContainer);
