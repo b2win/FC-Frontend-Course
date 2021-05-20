4eb006ac19f562cd9ec7eb018a9faefe5243f47c
b2win <68144560+b2win@users.noreply.github.com>
2021-05-20 22:05:53 +0900

    18장 redux-saga 예제 학습 완료
    18장 redux-saga 예제 학습 완료

--- react/study/learn-redux-middleware0518/src/modules/counter.js
+++ react/study/learn-redux-middleware0518/src/modules/counter.js
@@ -1,32 +1,53 @@
 import { createAction, handleActions } from "redux-actions";
+import {
+  delay,
+  put,
+  takeEvery,
+  takeLatest,
+  select,
+  throttle,
+} from "redux-saga/effects";
 
 const INCREASE = "counter/INCREASE";
 const DECREASE = "counter/DECREASE";
 
-// export const increase = createAction(INCREASE);
-// export const decrease = createAction(DECREASE);
-export const increase = () => ({ type: INCREASE });
-export const decrease = () => ({ type: DECREASE });
+const INCREASE_ASYNC = "counter/INCREASE_ASYNC";
+const DECREASE_ASYNC = "counter/DECREASE_ASYNC";
 
-export const increaseAsync = () => (dispatch) => {
-  setTimeout(() => {
-    dispatch(increase());
-  }, 1000);
-};
-
-export const decreaseAsync = () => (dispatch) => {
-  setTimeout(() => {
-    dispatch(decrease());
-  }, 1000);
-};
-
-// export const increase = () => (dispatch) => {
+// export const increaseAsync = () => (dispatch) => {
+//   setTimeout(() => {
 //     dispatch(increase());
+//   }, 1000);
 // };
 
-// export const decrease = () => (dispatch) => {
+// export const decreaseAsync = () => (dispatch) => {
+//   setTimeout(() => {
 //     dispatch(decrease());
+//   }, 1000);
 // };
+
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
+export const increaseAsync = createAction(INCREASE_ASYNC, () => undefined);
+export const decreaseAsync = createAction(DECREASE_ASYNC, () => undefined);
+
+function* increaseSaga() {
+  yield delay(1000);
+  yield put(increase());
+  const number = yield select((state) => state.counter);
+  console.log(`현재 값은 ${number}입니다.`);
+}
+
+function* decreaseSaga() {
+  yield delay(1000);
+  yield put(decrease());
+}
+
+export function* counterSaga() {
+  yield throttle(3000, INCREASE_ASYNC, increaseSaga);
+  // yield takeEvery(INCREASE_ASYNC, increaseSaga);
+  yield takeLatest(DECREASE_ASYNC, decreaseSaga);
+}
 
 const initialState = 1;
 
@@ -50,3 +71,56 @@ export default function counter(state = initialState, action) {
       return state;
   }
 }
+
+// import { createAction, handleActions } from "redux-actions";
+
+// const INCREASE = "counter/INCREASE";
+// const DECREASE = "counter/DECREASE";
+
+// // export const increase = createAction(INCREASE);
+// // export const decrease = createAction(DECREASE);
+// export const increase = () => ({ type: INCREASE });
+// export const decrease = () => ({ type: DECREASE });
+
+// export const increaseAsync = () => (dispatch) => {
+//   setTimeout(() => {
+//     dispatch(increase());
+//   }, 1000);
+// };
+
+// export const decreaseAsync = () => (dispatch) => {
+//   setTimeout(() => {
+//     dispatch(decrease());
+//   }, 1000);
+// };
+
+// // export const increase = () => (dispatch) => {
+// //     dispatch(increase());
+// // };
+
+// // export const decrease = () => (dispatch) => {
+// //     dispatch(decrease());
+// // };
+
+// const initialState = 1;
+
+// // const counter = handleActions(
+// //   {
+// //     [INCREASE]: (state) => state + 1,
+// //     [DECREASE]: (state) => state - 1,
+// //   },
+// //   initialState
+// // );
+
+// // export default counter;
+
+// export default function counter(state = initialState, action) {
+//   switch (action.type) {
+//     case INCREASE:
+//       return state + 1;
+//     case DECREASE:
+//       return state - 1;
+//     default:
+//       return state;
+//   }
+// }
--- react/study/learn-redux-middleware0518/src/modules/sample.js
+++ react/study/learn-redux-middleware0518/src/modules/sample.js
@@ -1,5 +1,9 @@
 import * as api from "../lib/api";
-import createRequestThunk from "../lib/createRequestThunk";
+import { call, put, takeLatest } from "redux-saga/effects";
+import { startLoading, finishLoading } from "./loading";
+
+import createRequestSaga from "../lib/createRequestSaga";
+import { createAction } from "redux-actions";
 
 // 액션 타입을 선언. 한 요청당 세 개를 만듬.
 const GET_POST = "sample/GET_POST";
@@ -8,8 +12,16 @@ const GET_POST_SUCCESS = "sample/GET_POST_SUCCESS";
 const GET_USERS = "sample/GET_USERS";
 const GET_USERS_SUCCESS = "sample/GET_USERS_SUCCESS";
 
-export const getPost = createRequestThunk(GET_POST, api.getPost);
-export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
+export const getPost = createAction(GET_POST, (id) => id);
+export const getUsers = createAction(GET_USERS);
+
+const getPostSaga = createRequestSaga(GET_POST, api.getPost);
+const getUsersSaga = createRequestSaga(GET_USERS, api.getUsers);
+
+export function* sampleSaga() {
+  yield takeLatest(GET_POST, getPostSaga);
+  yield takeLatest(GET_USERS, getUsersSaga);
+}
 
 // // thunk 함수를 생성. thunk 함수 내부에서는 시작할 때, 성공했을 때, 실패했을 때 다른 액션을 디스패치 함.
 // export const getPost = (id) => async (dispatch) => {
@@ -59,12 +71,12 @@ function sample(state = initialState, action) {
     case GET_POST_SUCCESS:
       return {
         ...state,
-        post: action.아무거나,
+        post: action.payload,
       };
     case GET_USERS_SUCCESS:
       return {
         ...state,
-        users: action.아무거나,
+        users: action.payload,
       };
     default:
       return state;
@@ -72,6 +84,81 @@ function sample(state = initialState, action) {
 }
 
 export default sample;
+
+// import * as api from "../lib/api";
+// import createRequestThunk from "../lib/createRequestThunk";
+
+// // 액션 타입을 선언. 한 요청당 세 개를 만듬.
+// const GET_POST = "sample/GET_POST";
+// const GET_POST_SUCCESS = "sample/GET_POST_SUCCESS";
+
+// const GET_USERS = "sample/GET_USERS";
+// const GET_USERS_SUCCESS = "sample/GET_USERS_SUCCESS";
+
+// export const getPost = createRequestThunk(GET_POST, api.getPost);
+// export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
+
+// // // thunk 함수를 생성. thunk 함수 내부에서는 시작할 때, 성공했을 때, 실패했을 때 다른 액션을 디스패치 함.
+// // export const getPost = (id) => async (dispatch) => {
+// //   dispatch({ type: GET_POST }); // 요청을 시작한 것을 알림
+// //   try {
+// //     const response = await api.getPost(id);
+// //     dispatch({
+// //       type: GET_POST_SUCCESS,
+// //       아무거나: response.data,
+// //     }); // 요청 성공
+// //   } catch (e) {
+// //     dispatch({
+// //       type: GET_POST_FAILURE,
+// //       아무거나: e,
+// //       error: true,
+// //     }); // 에러 발생
+// //     throw e;
+// //   }
+// // };
+
+// // export const getUsers = () => async (dispatch) => {
+// //   dispatch({ type: GET_USERS });
+// //   try {
+// //     const response = await api.getUsers();
+// //     dispatch({
+// //       type: GET_USERS_SUCCESS,
+// //       아무거나: response.data,
+// //     }); // 요청 성공
+// //   } catch (e) {
+// //     dispatch({
+// //       type: GET_USERS_FAILURE,
+// //       아무거나: e,
+// //       error: true,
+// //     }); // 에러 발생
+// //     throw e;
+// //   }
+// // };
+
+// // 초기 상태를 선언. 요청의 로딩 중 상태는 loading이라는 객체에서 관리.
+// const initialState = {
+//   post: null,
+//   users: null,
+// };
+
+// function sample(state = initialState, action) {
+//   switch (action.type) {
+//     case GET_POST_SUCCESS:
+//       return {
+//         ...state,
+//         post: action.아무거나,
+//       };
+//     case GET_USERS_SUCCESS:
+//       return {
+//         ...state,
+//         users: action.아무거나,
+//       };
+//     default:
+//       return state;
+//   }
+// }
+
+// export default sample;
 
 // import { handleActions } from "redux-actions";
 // import * as api from "../lib/api";
