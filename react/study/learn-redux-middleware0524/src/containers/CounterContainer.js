1c15a747e02189c6f8146bd8135d7edd111bb4ee
b2win <68144560+b2win@users.noreply.github.com>
2021-05-24 21:00:29 +0900

    18.2장 예제 복습 완료
    18.2장 예제 복습 완료

--- react/study/learn-redux-middleware0524/src/containers/CounterContainer.js
+++ react/study/learn-redux-middleware0524/src/containers/CounterContainer.js
@@ -3,30 +3,30 @@ import { connect, useDispatch, useSelector } from "react-redux";
 import Counter from "../components/Counter";
 import { increase, decrease } from "../modules/counter";
 
-const CounterContainer = () => {
-  const number = useSelector((state) => state.counter);
-  // state.counter.number가 아닌 이유 = initialState에 숫자 0으로 작성, (number: ... 으로 안함)
+// const CounterContainer = () => {
+//   const number = useSelector((state) => state.counter);
+//   // state.counter.number가 아닌 이유 = initialState에 숫자 0으로 작성, (number: ... 으로 안함)
 
-  const dispatch = useDispatch();
-  const increase = useCallback(() => dispatch(increase()), [dispatch]);
-  const decrease = useCallback(() => dispatch(decrease()), [dispatch]);
+//   const dispatch = useDispatch();
+//   const increase = useCallback(() => dispatch(increase()), [dispatch]);
+//   const decrease = useCallback(() => dispatch(decrease()), [dispatch]);
 
-  return (
-    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
-  );
-};
-
-export default CounterContainer;
-
-// const CounterContainer = ({ number, increase, decrease }) => {
 //   return (
 //     <Counter number={number} onIncrease={increase} onDecrease={decrease} />
 //   );
 // };
 
-// export default connect(
-//   (state) => ({
-//     number: state.counter,
-//   }),
-//   { increase, decrease }
-// )(CounterContainer);
+// export default CounterContainer;
+
+const CounterContainer = ({ number, increase, decrease }) => {
+  return (
+    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+  );
+};
+
+export default connect(
+  (state) => ({
+    number: state.counter,
+  }),
+  { increase, decrease }
+)(CounterContainer);
--- react/study/learn-redux-middleware0524/src/index.js
+++ react/study/learn-redux-middleware0524/src/index.js
@@ -4,10 +4,14 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import rootReducer from "./modules";
+// import loggerMiddleware from "./lib/loggerMiddleware";
+import { createLogger } from "redux-logger";
 
-const store = createStore(rootReducer);
+const logger = createLogger();
+
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 ReactDOM.render(
   <Provider store={store}>
