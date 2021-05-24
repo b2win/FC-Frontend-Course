df81e94c0ebea079962ebc4650842d0ac5f64535
b2win <68144560+b2win@users.noreply.github.com>
2021-05-24 21:35:11 +0900

    18장 미들웨어 thunk 예제 진행중
    18장 미들웨어 thunk 예제 진행중

--- react/study/learn-redux-middleware0524/src/containers/CounterContainer.js
+++ react/study/learn-redux-middleware0524/src/containers/CounterContainer.js
@@ -1,7 +1,29 @@
 import { useCallback } from "react";
 import { connect, useDispatch, useSelector } from "react-redux";
 import Counter from "../components/Counter";
-import { increase, decrease } from "../modules/counter";
+import {
+  increase,
+  decrease,
+  increaseAsync,
+  decreaseAsync,
+} from "../modules/counter";
+
+const CounterContainer = ({ number, increaseAsync, decreaseAsync }) => {
+  return (
+    <Counter
+      number={number}
+      onIncrease={increaseAsync}
+      onDecrease={decreaseAsync}
+    />
+  );
+};
+
+export default connect(
+  (state) => ({
+    number: state.counter,
+  }),
+  { increaseAsync, decreaseAsync }
+)(CounterContainer);
 
 // const CounterContainer = () => {
 //   const number = useSelector((state) => state.counter);
@@ -17,16 +39,3 @@ import { increase, decrease } from "../modules/counter";
 // };
 
 // export default CounterContainer;
-
-const CounterContainer = ({ number, increase, decrease }) => {
-  return (
-    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
-  );
-};
-
-export default connect(
-  (state) => ({
-    number: state.counter,
-  }),
-  { increase, decrease }
-)(CounterContainer);
--- react/study/learn-redux-middleware0524/src/modules/counter.js
+++ react/study/learn-redux-middleware0524/src/modules/counter.js
@@ -3,13 +3,37 @@ import { createAction, handleActions } from "redux-actions";
 const INCREASE = "counter/INCREASE";
 const DECREASE = "counter/DECREASE";
 
-// export const increase = () => ({ type: INCREASE });
-// export const decrease = () => ({ type: DECREASE });
-
 export const increase = createAction(INCREASE);
 export const decrease = createAction(DECREASE);
 
+export const increaseAsync = () => (dispatch) => {
+  setTimeout(() => {
+    dispatch(increase());
+  }, 1000);
+};
+
+export const decreaseAsync = () => (dispatch) => {
+  setTimeout(() => {
+    dispatch(decrease());
+  }, 1000);
+};
+
 const initialState = 0;
+
+const counter = handleActions(
+  {
+    [INCREASE]: (state) => state + 1,
+    [DECREASE]: (state) => state - 1,
+  },
+  initialState
+);
+
+export default counter;
+
+// export const increase = () => ({ type: INCREASE });
+// export const decrease = () => ({ type: DECREASE });
+
+// const initialState = 0;
 
 // function counter(state = initialState, action) {
 //   switch (action.type) {
@@ -26,12 +50,4 @@ const initialState = 0;
 //   }
 // }
 
-const counter = handleActions(
-  {
-    [INCREASE]: (state) => state + 1,
-    [DECREASE]: (state) => state - 1,
-  },
-  initialState
-);
-
-export default counter;
+// export default counter;
