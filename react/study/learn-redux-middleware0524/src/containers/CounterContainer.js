04b83911276f63b0251d7a90bd2d0e3c88646987
b2win <68144560+b2win@users.noreply.github.com>
2021-05-24 20:44:43 +0900

    18.1장 hooks 방법으로 변경 완료
    18.1장 hooks 방법으로 변경 완료

--- react/study/learn-redux-middleware0524/src/containers/CounterContainer.js
+++ react/study/learn-redux-middleware0524/src/containers/CounterContainer.js
@@ -1,16 +1,32 @@
-import { connect } from "react-redux";
+import { useCallback } from "react";
+import { connect, useDispatch, useSelector } from "react-redux";
 import Counter from "../components/Counter";
 import { increase, decrease } from "../modules/counter";
 
-const CounterContainer = ({ number, increase, decrease }) => {
+const CounterContainer = () => {
+  const number = useSelector((state) => state.counter);
+  // state.counter.number가 아닌 이유 = initialState에 숫자 0으로 작성, (number: ... 으로 안함)
+
+  const dispatch = useDispatch();
+  const increase = useCallback(() => dispatch(increase()), [dispatch]);
+  const decrease = useCallback(() => dispatch(decrease()), [dispatch]);
+
   return (
     <Counter number={number} onIncrease={increase} onDecrease={decrease} />
   );
 };
 
-export default connect(
-  (state) => ({
-    number: state.counter,
-  }),
-  { increase, decrease }
-)(CounterContainer);
+export default CounterContainer;
+
+// const CounterContainer = ({ number, increase, decrease }) => {
+//   return (
+//     <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+//   );
+// };
+
+// export default connect(
+//   (state) => ({
+//     number: state.counter,
+//   }),
+//   { increase, decrease }
+// )(CounterContainer);
--- react/study/learn-redux-middleware0524/src/modules/counter.js
+++ react/study/learn-redux-middleware0524/src/modules/counter.js
@@ -3,17 +3,35 @@ import { createAction, handleActions } from "redux-actions";
 const INCREASE = "counter/INCREASE";
 const DECREASE = "counter/DECREASE";
 
-export const increase = createAction(INCREASE);
-export const decrease = createAction(DECREASE);
+export const increase = () => ({ type: INCREASE });
+export const decrease = () => ({ type: DECREASE });
+
+// export const increase = createAction(INCREASE);
+// export const decrease = createAction(DECREASE);
 
 const initialState = 0;
 
-const counter = handleActions(
-  {
-    [INCREASE]: (state) => state + 1,
-    [DECREASE]: (state) => state - 1,
-  },
-  initialState
-);
+function counter(state = initialState, action) {
+  switch (action.type) {
+    case INCREASE:
+      return {
+        number: state.number + 1,
+      };
+    case DECREASE:
+      return {
+        number: state.number - 1,
+      };
+    default:
+      return state;
+  }
+}
+
+// const counter = handleActions(
+//   {
+//     [INCREASE]: (state) => state + 1,
+//     [DECREASE]: (state) => state - 1,
+//   },
+//   initialState
+// );
 
 export default counter;
