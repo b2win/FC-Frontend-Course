c8ae56b151e282713a92aba7a1476e5a1ca15252
b2win <68144560+b2win@users.noreply.github.com>
2021-05-24 18:16:08 +0900

    17장 예제 후반부 복습 완료
    17장 예제 후반부 복습 완료

--- react/study/react-redux-tutorial0523/src/containers/CounterContainers.js
+++ react/study/react-redux-tutorial0523/src/containers/CounterContainers.js
@@ -1,18 +1,38 @@
-import { connect } from "react-redux";
+import { useCallback } from "react";
+import { connect, useDispatch, useSelector } from "react-redux";
 // import { bindActionCreators } from "redux";
 import Counter from "../components/Counter";
 import { increase, decrease } from "../modules/counter";
 
-const CounterContainer = ({ number, increase, decrease }) => {
+const CounterContainer = () => {
+  const number = useSelector((state) => state.counter.number);
+  const dispatch = useDispatch();
+
+  const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+  const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
   return (
-    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+    <Counter
+      number={number}
+      onIncrease={onIncrease}
+      onDecrease={onDecrease}
+      // onIncrease={() => dispatch(increase())}
+      // onDecrease={() => dispatch(decrease())}
+    />
   );
 };
 
-export default connect((state) => ({ number: state.counter.number }), {
-  increase,
-  decrease,
-})(CounterContainer);
+export default CounterContainer;
+
+// const CounterContainer = ({ number, increase, decrease }) => {
+//   return (
+//     <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+//   );
+// };
+
+// export default connect((state) => ({ number: state.counter.number }), {
+//   increase,
+//   decrease,
+// })(CounterContainer);
 
 // export default connect(
 //   (state) => ({ number: state.counter.number }),
