e9c907a9194c4490103203ff2e43ca32760ac23b
b2win <68144560+b2win@users.noreply.github.com>
2021-05-24 13:03:41 +0900

    17장 예제 복습 전반부(~17.5)
    17장 예제 복습 전반부(~17.5)

---
+++ react/study/react-redux-tutorial0523/src/containers/CounterContainers.js
@@ -0,0 +1,37 @@
+import { connect } from "react-redux";
+// import { bindActionCreators } from "redux";
+import Counter from "../components/Counter";
+import { increase, decrease } from "../modules/counter";
+
+const CounterContainer = ({ number, increase, decrease }) => {
+  return (
+    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+  );
+};
+
+export default connect((state) => ({ number: state.counter.number }), {
+  increase,
+  decrease,
+})(CounterContainer);
+
+// export default connect(
+//   (state) => ({ number: state.counter.number }),
+//   (dispatch) =>
+//     bindActionCreators(
+//       {
+//         increase,
+//         decrease,
+//       },
+//       dispatch
+//     )
+// )(CounterContainer);
+
+// export default connect(
+//   (state) => ({ number: state.counter.number }),
+//   (dispatch) => ({
+//     increase: () => dispatch(increase()),
+//     decrease: () => dispatch(decrease()),
+//   })
+// )(CounterContainer);
+
+// export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
