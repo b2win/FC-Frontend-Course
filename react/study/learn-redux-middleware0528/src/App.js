fd4552ac14919d69f16757cddd363ef5d60bb3ad
b2win <68144560+b2win@users.noreply.github.com>
2021-05-28 23:48:36 +0900

    18장 미들웨어 thunk 예제 복습
    18장 미들웨어 thunk 예제 복습

---
+++ react/study/learn-redux-middleware0528/src/App.js
@@ -0,0 +1,12 @@
+import CounterContainers from "./containers/CounterContainers";
+import SampleContainer from "./containers/Sample.Container";
+
+function App() {
+  return (
+    <div>
+      <SampleContainer />
+    </div>
+  );
+}
+
+export default App;
---
+++ react/study/learn-redux-middleware0528/src/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import rootReducer from "./modules";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import { createLogger } from "redux-logger";
+import ReduxThunk from "redux-thunk";
+// import loggerMiddleware from "./lib/loggerMiddleware";
+
+const logger = createLogger();
+const store = createStore(rootReducer, applyMiddleware(logger, ReduxThunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
+  document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
