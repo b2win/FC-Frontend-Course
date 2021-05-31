189e9f6d3a8cac6658377e2d57eb1de2fc5244f4
b2win <68144560+b2win@users.noreply.github.com>
2021-05-31 12:01:22 +0900

    FC 타입스크립트 연습 A - 작업환경 설정, 기본타입, 함수 예제 코드
    FC  타입스크립트 연습 A - 작업환경 설정, 기본타입, 함수 예제 코드

---
+++ react/study/ts-practice/dist/practice.js
@@ -0,0 +1,16 @@
+"use strict";
+// const message: string = "hello world!"
+// console.log(message)
+let count = 0;
+count += 1;
+// count = "문자열";
+const message = "hello world";
+const done = false;
+const numbers = [1, 2, 3];
+const messages = ["hello", "world"];
+// message.push(1);
+let mightBeUndefined = undefined;
+let nullableNumber = null;
+let color = "red";
+color = "yellow";
+// color = "green";
