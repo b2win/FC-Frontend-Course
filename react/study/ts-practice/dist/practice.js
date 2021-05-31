8293807ef5958838520b4ac931efc055b3bda7b8
b2win <68144560+b2win@users.noreply.github.com>
2021-05-31 12:35:55 +0900

    FC 타입스크립트 연습 C - Generics 예제 코드
    FC 타입스크립트 연습 C - Generics 예제 코드

--- react/study/ts-practice/dist/practice.js
+++ react/study/ts-practice/dist/practice.js
@@ -14,3 +14,94 @@ let nullableNumber = null;
 let color = "red";
 color = "yellow";
 // color = "green";
+function sum(x, y) {
+    return x + y;
+}
+const result = sum(1, 2);
+function sumArray(numbers) {
+    return numbers.reduce((acc, current) => acc + current, 0);
+}
+const total = sumArray([1, 2, 3, 4, 5]);
+console.log(total);
+function returnStringOrNumber() {
+    return 4;
+}
+class Circle {
+    // radius: number;
+    // constructor(radius: number) {
+    //   this.radius = radius;
+    constructor(radius) {
+        this.radius = radius;
+    }
+    getArea() {
+        return this.radius * this.radius * Math.PI;
+    }
+}
+class Rectangle {
+    // width: number;
+    // height: number;
+    // constructor(width: number, height: number) {
+    //   this.width = width;
+    //   this.height = height;
+    // }
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+    getArea() {
+        return this.width * this.height;
+    }
+}
+const circle = new Circle(5);
+const rectangle = new Rectangle(2, 5);
+const shapes = [circle, rectangle];
+shapes.forEach(shape => {
+    console.log(shape.getArea());
+});
+const person = {
+    name: "사람이요",
+    age: 20,
+};
+const expert = {
+    name: "개발이요",
+    skills: ["JavaScript", "react", "typescript"]
+};
+//Generics
+function merge(a, b) {
+    return Object.assign(Object.assign({}, a), b);
+}
+const merged = merge({ foo: 1 }, { bar: 2, foobar: 3 });
+function wrap(param) {
+    return {
+        param
+    };
+}
+const wrapped = wrap("aaa");
+wrapped.param;
+const items = {
+    list: [1, 2, 3],
+    value: "abc",
+};
+class Queue {
+    constructor() {
+        this.list = [];
+    }
+    get length() {
+        return this.list.length;
+    }
+    enqueue(item) {
+        this.list.push(item);
+    }
+    dequeue() {
+        return this.list.shift();
+    }
+}
+const queue = new Queue();
+queue.enqueue(0);
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+queue.enqueue(4);
+while (queue.length > 0) {
+    console.log(queue.dequeue());
+}
--- react/study/ts-practice/src/practice.ts
+++ react/study/ts-practice/src/practice.ts
@@ -98,3 +98,65 @@ const expert: Developer = {
   name: "개발이요",
   skills: ["JavaScript", "react", "typescript"]
 }
+
+
+//Generics
+function merge<T1, T2>(a: T1, b: T2) {
+  return {
+    ...a,
+    ...b
+  };
+}
+
+const merged = merge({foo : 1}, {bar : 2, foobar : 3})
+
+function wrap<T>(param: T) {
+  return {
+    param
+  };
+}
+
+const wrapped = wrap("aaa");
+
+wrapped.param
+
+// interface Items<T> {
+//   list: T[];
+// }
+type Items<T, V> = {
+  list: T[];
+  value: V
+}
+
+const items: Items<number, string> = {
+  list: [1, 2, 3],
+  value: "abc",
+};
+
+
+class Queue<T> {
+  list: T[] = [];
+
+  get length() {
+    return this.list.length;
+  }
+
+  enqueue(item: T) {
+    this.list.push(item);
+  }
+
+  dequeue() {
+    return this.list.shift();
+  }
+}
+
+const queue = new Queue<number>();
+queue.enqueue(0);
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+queue.enqueue(4);
+
+while (queue.length > 0) {
+  console.log(queue.dequeue());
+}
