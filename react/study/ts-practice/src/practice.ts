a545b4559b98498d5c102b4fc8418c50839fc465
b2win <68144560+b2win@users.noreply.github.com>
2021-05-31 12:22:25 +0900

    FC 타임스크립트 연습 B - Interface, Type Alias 예제 코드
    FC 타임스크립트 연습 B - Interface, Type Alias 예제 코드

--- react/study/ts-practice/src/practice.ts
+++ react/study/ts-practice/src/practice.ts
@@ -38,3 +38,63 @@ console.log(total);
 function returnStringOrNumber(): string | number {
   return 4;
 }
+
+interface Shape {
+  getArea(): number;
+}
+
+class Circle implements Shape {
+  // radius: number;
+
+  // constructor(radius: number) {
+  //   this.radius = radius;
+  constructor(public radius: number){
+  }
+  getArea() {
+    return this.radius * this.radius * Math.PI;
+  }
+}
+
+class Rectangle implements Shape {
+  // width: number;
+  // height: number;
+  // constructor(width: number, height: number) {
+  //   this.width = width;
+  //   this.height = height;
+  // }
+  constructor(private width: number, private height: number){
+  }
+  getArea() {
+    return this.width * this.height;
+  }
+}
+
+const circle = new Circle(5);
+const rectangle = new Rectangle(2, 5);
+const shapes: Shape[] = [circle, rectangle];
+
+shapes.forEach(shape => {
+  console.log(shape.getArea())
+})
+
+interface Person {
+  name: string;
+  age?: number;
+}
+
+// interface Developer {
+  // name: string;
+  // age?: number;
+  interface Developer extends Person {
+  skills: string[];
+}
+
+const person: Person = {
+  name: "사람이요",
+  age: 20,
+}
+
+const expert: Developer = {
+  name: "개발이요",
+  skills: ["JavaScript", "react", "typescript"]
+}
