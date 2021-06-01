2a5377c373d0d909d72ee53d153d0c7313d5c325
b2win <68144560+b2win@users.noreply.github.com>
2021-06-01 13:19:56 +0900

    FC 타입스크립트로 리액트 상태 관리 A - useState, 이벤트 예제 코드
    FC 타입스크립트로 리액트 상태 관리 A - useState, 이벤트 예제 코드

--- react/study/ts-react-tutorial/src/App.tsx
+++ react/study/ts-react-tutorial/src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
-import Greetings from './Greetings';
+// import Counter from './Counter';
+// import Greetings from './Greetings';
+import MyForm from './MyForm';
 
 function App() {
-  const onClick = (name: string) => {
-    console.log(name)
-  }
-  return (
-<Greetings name="리액트" onClick={onClick} />
-  );
-}
+  const onSubmit = (form: {name: string; description: string }) => {
+    console.log(form);
+  };
+  return <MyForm onSubmit={onSubmit} />
 
+}
 export default App;
