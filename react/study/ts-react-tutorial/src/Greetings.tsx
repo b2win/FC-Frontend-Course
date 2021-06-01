2f388b9900aaebf07797c87b6bbb8e258f592c75
b2win <68144560+b2win@users.noreply.github.com>
2021-06-01 11:51:46 +0900

    FC 타입스크립트로 리액트 컴포넌트 만들기 예제 코드
    FC 타입스크립트로 리액트 컴포넌트 만들기 예제 코드

---
+++ react/study/ts-react-tutorial/src/Greetings.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+type GreetingsProps = {
+  name: string;
+  mark: string;
+  optional? : string;
+  onClick: (name: string) => void;
+};
+
+function Greetings({ name, mark, optional, onClick}: GreetingsProps) {
+  const handleClick = () => onClick(name);
+  return (
+    <div>
+      Hello, {name} {mark}
+      {optional && <p>{optional}</p>}
+      <div>
+        <button onClick={handleClick}>Click Me</button>
+      </div>
+    </div>
+  );
+  };
+
+  Greetings.defaultProps = {
+    mark: "!"
+  };
+  
+
+// const Greetings: React.FC<GreetingsProps> = ({ name, mark = "!"}) => {
+// return (
+//   <div>
+//     Hello, {name} {mark}
+//   </div>
+// );
+// };
+
+
+
+export default Greetings;
