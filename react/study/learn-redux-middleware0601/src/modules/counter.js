a7e89ad29724f0ae87227e3bb736461d0c7b23c8
b2win <68144560+b2win@users.noreply.github.com>
2021-06-01 16:23:47 +0900

    18장 예제 틀 준비
    18장 예제 틀 준비

---
+++ react/study/learn-redux-middleware0601/src/modules/counter.js
@@ -0,0 +1,32 @@
+import { createAction, handleActions } from "redux-actions";
+
+const INCREASE = "counter/INCREASE";
+const DECREASE = "counter/DECREASE";
+
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
+// export const increase = () => ({ type: INCREASE });
+// export const decrease = () => ({ type: DECREASE });
+
+const initialState = 0;
+
+const counter = handleActions(
+  {
+    [INCREASE]: (state) => state + 1,
+    [DECREASE]: (state) => state - 1,
+  },
+  initialState
+);
+
+// function counter(state = initialState, action) {
+//   switch (action.type) {
+//     case INCREASE:
+//       return state + 1;
+//     case DECREASE:
+//       return state - 1;
+//     default:
+//       state;
+//   }
+// }
+
+export default counter;
