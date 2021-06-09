58a2a74a9d98730b83a6782d5cc64faa2d648b37
b2win <68144560+b2win@users.noreply.github.com>
2021-06-09 17:56:27 +0900

    20.4.6 redux-saga 코드 준비하기 예제 완료
    20.4.6 redux-saga 코드 준비하기 예제 완료

--- react/study/ssr-recipe0608/src/index.js
+++ react/study/ssr-recipe0608/src/index.js
@@ -5,15 +5,20 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import rootReducer from "./modules";
 import { applyMiddleware, createStore } from "redux";
 import thunk from "redux-thunk";
+import createSagaMiddleware from "redux-saga";
+import rootReducer, { rootSaga } from "./modules";
+
+const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
   rootReducer,
   window.__PRELOADED_STATE__, // 이 값을 초기 상태로 사용.
-  applyMiddleware(thunk)
+  applyMiddleware(thunk, sagaMiddleware)
 );
+
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
   <Provider store={store}>
