55ef7b4c9416f46da530c54d3b7bfd1e1b871c93
b2win <68144560+b2win@users.noreply.github.com>
2021-06-11 23:08:48 +0900

    20.5 예제 코드
    20.5 예제 코드

--- react/study/ssr-recipe0610/src/index.js
+++ react/study/ssr-recipe0610/src/index.js
@@ -4,33 +4,42 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 import { applyMiddleware, createStore } from "redux";
 import thunk from "redux-thunk";
-import { Provider } from "react-redux";
 import createSagaMiddleware from "redux-saga";
 import rootReducer, { rootSaga } from "./modules";
+import { loadableReady } from "@loadable/component";
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
   rootReducer,
-  window.__PRELOADED_STATE__,
+  window.__PRELOADED_STATE__, // 이 값을 초기 상태로 사용.
   applyMiddleware(thunk, sagaMiddleware)
 );
 
-sagaMiddleware.run(rootSaga);
-
-ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <React.StrictMode>
+// 같은 내용을 쉽게 재사용할 수 있도록 렌더링할 내용을 하나의 컴포넌트로 묶음
+const Root = () => {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
         <App />
-      </React.StrictMode>
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById("root")
-);
+      </BrowserRouter>
+    </Provider>
+  );
+};
+
+const root = document.getElementById("root");
+
+// 프로덕션 환경에서는 loadableReady와 hydrate를 사용하고
+// 개발 환경에서는 기존 방식으로 처리
+if (process.env.NODE_ENV === "production") {
+  loadableReady(() => {
+    ReactDOM.hydrate(<Root />, root);
+  });
+} else {
+  ReactDOM.render(<Root />, root);
+}
 
 reportWebVitals();
-
-// git commit -m'Commit before eject'
