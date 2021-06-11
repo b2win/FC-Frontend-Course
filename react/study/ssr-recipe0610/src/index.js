ea8edc36e0f99c8f2bb00c0aa0ad27538599c1a2
b2win <68144560+b2win@users.noreply.github.com>
2021-06-11 22:37:53 +0900

    20.4 예제 코드
    20.4 예제 코드

--- react/study/ssr-recipe0610/src/index.js
+++ react/study/ssr-recipe0610/src/index.js
@@ -4,16 +4,21 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import rootReducer from "./modules";
 import { applyMiddleware, createStore } from "redux";
 import thunk from "redux-thunk";
 import { Provider } from "react-redux";
+import createSagaMiddleware from "redux-saga";
+import rootReducer, { rootSaga } from "./modules";
+
+const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
   rootReducer,
   window.__PRELOADED_STATE__,
-  applyMiddleware(thunk)
+  applyMiddleware(thunk, sagaMiddleware)
 );
+
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
   <Provider store={store}>
